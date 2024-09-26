5c190b06b287c18a82084efff3cf4b1249fb2eea
izza <muhammadibnzzi@gmail.com>
2024-09-26 09:00:03 +0700

    pemilihanBilangan-2

--- PemilihanBilangan19.java
+++ PemilihanBilangan19.java
@@ -8,10 +8,14 @@ public class PemilihanBilangan19 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }else{
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.print(hasil);
+        
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // }else{
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
+
     }
 }
